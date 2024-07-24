61d03c1ab7efedf91dd47aa1b541fb9fefc895ba
Liri <lirisobol16@gmail.com>
2024-07-24 13:36:29 +0300

    created a LoadingSpinner component

---
+++ fpl-stats/src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,16 @@
+import { Spinner } from "react-bootstrap"
+import styles from "../LoadingSpinner/LoadingSpinner.module.scss";
+export function LoadingSpinner():JSX.Element {
+    return (
+        <div className={styles.LoadingSpinnerContainer}>
+            <Spinner 
+                animation="border" 
+                variant="primary" 
+                style={{
+                    height:"5rem",
+                    width:"5rem"
+                }}
+                />            
+        </div>
+    )
+}
