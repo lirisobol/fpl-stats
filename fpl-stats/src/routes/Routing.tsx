4ac50a4ea7d4d4d3f91c4ac272dfd56c51e0d230
Liri <lirisobol16@gmail.com>
2024-07-25 19:22:14 +0300

    minor naming changes

--- fpl-stats/src/routes/Routing.tsx
+++ fpl-stats/src/routes/Routing.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
 import { Teams } from "../pages/Teams/Teams";
 import { Fixtures } from "../pages/Fixtures/Fixtures";
-import { Players } from "../pages/Players/Players";
+import { PlayersByTeam } from "../pages/PlayersByTeam/Players";
 export function Routing():JSX.Element {
     return(
         <>
@@ -10,7 +10,7 @@ export function Routing():JSX.Element {
                 <Route path="/" element={<Home />} />
                 <Route path="/teams" element={<Teams />} />
                 <Route path="/fixtures" element={<Fixtures />} />
-                <Route path="/teams/:teamCode" element={<Players />} />
+                <Route path="/teams/:teamCode" element={<PlayersByTeam />} />
             </Routes>
         </>
     )
