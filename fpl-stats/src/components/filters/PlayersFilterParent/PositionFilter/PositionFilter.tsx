2a578799ff4566253e04daa9cd9bdae9ce7e9114
Liri <lirisobol16@gmail.com>
2024-07-25 19:55:48 +0300

    replaced toggle buttons with select menus

--- fpl-stats/src/components/filters/PlayersFilterParent/PositionFilter/PositionFilter.tsx
+++ fpl-stats/src/components/filters/PlayersFilterParent/PositionFilter/PositionFilter.tsx
@@ -1,74 +1,52 @@
-import { ButtonGroup, ToggleButton } from "react-bootstrap";
 import { useAppDispatch, useAppSelector } from "../../../../hooks/redux-hooks";
 import { setPositionType } from "../../../../store/slices/filterSlice";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPowerOff,faHands,faShield,faCircleNodes,faBullseye} from "@fortawesome/free-solid-svg-icons";
 
-interface PositionType {
-    id:number,
-    plural_name: string,
-    plural_name_short:string
-}
-const PositionTypes: PositionType[] = [
-    {
-        id: 1,
-        plural_name: "Goalkeepers",
-        plural_name_short: "GKP",
-    },
-    {
-        id: 2,
-        plural_name: "Defenders",
-        plural_name_short: "DEF",
-    },
-    {
-        id: 3,
-        plural_name: "Midfielders",
-        plural_name_short: "MID",
-    },
-    {
-        id: 4,
-        plural_name: "Forwards",
-        plural_name_short: "FWD",
-    },
-]
 export function PositionFilter(): JSX.Element {
     const dispatch = useAppDispatch();
     const positionType = useAppSelector((state) => state.filters.positionType);
 
-    const handlePositionTypeChange = (newPositionType: number) => {
-        dispatch(setPositionType(newPositionType));
+    const handlePositionTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const positionType = Number(event.target.value);
+        console.log(positionType);
+        dispatch(setPositionType(positionType));
     };
-    // import { faPowerOff,faHands,faShield,faCircleNodes,faBullseye} from "@fortawesome/free-solid-svg-icons";
     return (
-        <ButtonGroup>
-            <ToggleButton
-                key="all-positions"
-                type="radio"
-                variant="outline-dark"
-                name="position-radio"
-                value={0}
-                checked={positionType === 0}
-                onChange={() => handlePositionTypeChange(0)}
-                id="all-positions-toggle"
+        <select value={positionType} onChange={handlePositionTypeChange} className="form-select">
+            <option
+                key='all-positions'
+                value='0'
+                id='all-positions'
             >
-                <FontAwesomeIcon icon={faPowerOff}/>
                 All
-            </ToggleButton>
-            {PositionTypes.map((type) => (
-                <ToggleButton
-                    key={type.id}
-                    type="radio"
-                    variant="outline-dark"
-                    name="position-radio"
-                    value={type.id}
-                    checked={positionType === type.id}
-                    onChange={() => handlePositionTypeChange(type.id)}
-                    id={`position-${type.plural_name}`}
-                    >
-                    <FontAwesomeIcon icon={faHands}/>
-                    {type.plural_name}
-                </ToggleButton>
-            ))}
-        </ButtonGroup>
+            </option>
+            <option
+                key='goalkeepers'
+                value='1'
+                id='goalkeepers'
+            >
+                Goalkeepers
+            </option>
+            <option
+                key='defenders'
+                value='2'
+                id='defenders'
+            >
+                Defenders
+            </option>
+            <option
+                key='midfielders'
+                value='3'
+                id='midfielders'
+            >
+                Midfielders
+            </option>
+            <option
+                key='attackers'
+                value='4'
+                id='attackers'
+            >
+                Attackers
+            </option>
+        </select>
     )
 }
