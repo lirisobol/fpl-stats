63cd0989a265ba8500a279761e22a0bfbbe28a4a
Liri <lirisobol16@gmail.com>
2024-07-28 14:16:54 +0300

    initial <PlayerSearchModal> setup

---
+++ fpl-stats/src/components/modals/PlayerSearchModal/PlayerSearchModal.tsx
@@ -0,0 +1,29 @@
+import { Button, Modal } from "react-bootstrap";
+
+interface PlayerSearchModalProps {
+    show: boolean;
+    onHide: () => void;
+}
+export function PlayerSearchModal(props: PlayerSearchModalProps) {
+
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Modal heading
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+  }
--- fpl-stats/src/pages/Compare/Compare.tsx
+++ fpl-stats/src/pages/Compare/Compare.tsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
 import styles from "./Compare.module.scss";
+import { Button } from "react-bootstrap";
+import { PlayerSearchModal } from "../../components/modals/PlayerSearchModal/PlayerSearchModal";
 export function Compare(): JSX.Element {
+    const [modalShow, setModalShow] = useState(false);
+    
+    const handleModalOpen = () => {
+        setModalShow(true);
+    }
+    const handleModalClose = () => {
+        setModalShow(false)
+    }
+
     return (
         <div className={styles.CompareWrapper}>
-            X
+            <Button
+                variant="primary"
+                onClick={handleModalOpen}
+            >
+                Add Players
+            </Button>
+
+            <PlayerSearchModal 
+                show={modalShow}
+                onHide={handleModalClose}
+            />
         </div>
     )
 }
