b62a0b2bfaf53bd2b98413ea5e161b86338e773a
Liri <lirisobol16@gmail.com>
2024-07-30 15:24:33 +0300

    separated column definitions to a TableDefs file

--- fpl-stats/src/components/tables/LeagueTable/LeagueTable.tsx
+++ fpl-stats/src/components/tables/LeagueTable/LeagueTable.tsx
@@ -5,37 +5,18 @@ import { useState, useEffect } from "react";
 import { useDynamicRowHeight } from "../../../hooks/useDynamicRowHeight";
 import { Team } from "../../../models/Team";
 import { ColDef } from "ag-grid-community";
-import { ViewPlayersButtons } from "./ViewPlayersButton/ViewPlayersButton";
+import { defaultColDef, generateLeagueColDef } from "./TableDefs";
 
 interface LeagueTableProps {
     teams: Team[];
 }
 export function LeagueTable({ teams }: LeagueTableProps): JSX.Element {
-    
     const { getRowHeight, onGridReady, onFirstDataRendered, onGridSizeChanged } = useDynamicRowHeight();
     const [columnDefs, setColumnDefs] = useState<ColDef[]>([]);
-
-    // Initialize column definitions using useEffect
     useEffect(() => {
-        const initialColumnDefs: ColDef[] = [
-            { headerName: 'ID', field: 'id' },
-            { headerName: 'Name', field: 'name' },
-            { 
-                headerName: 'Players', 
-                field: 'code', 
-                cellRenderer: ViewPlayersButtons // Use the string name of the component
-            }
-        ];
+        const initialColumnDefs = generateLeagueColDef();
         setColumnDefs(initialColumnDefs);
-    }, []); // Empty dependency array ensures this runs only once
-
-    // Default column properties (optional)
-    const defaultColDef: ColDef = {
-        sortable: true,
-        filter: true,
-        resizable: true,
-        flex: 1,
-    };
+    }, []); 
     return (
         <div className="ag-theme-quartz" style={{ height: 600, width: '100%', fontSize: "1rem" }}>
             <AgGridReact 
