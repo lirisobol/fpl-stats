7db20932e739b92f17a85607e3012f96d51d4e9c
Liri <lirisobol16@gmail.com>
2024-07-30 15:09:41 +0300

    added customized alerts & scss modules

--- fpl-stats/src/pages/Compare/Compare.tsx
+++ fpl-stats/src/pages/Compare/Compare.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import styles from './Compare.module.scss';
-import { Button, Alert } from 'react-bootstrap'; // Import Alert for messaging
+import { Alert, Button} from 'react-bootstrap'; // Import Alert for messaging
 import { PlayerSearchModal } from '../../components/modals/PlayerSearchModal/PlayerSearchModal';
 import { useAppSelector } from '../../hooks/redux-hooks';
 import { PlayerCompareTable } from '../../components/tables/PlayerCompareTable/PlayerCompareTable';
 import { PlayerData } from '../../models/Player';
+import { WarningAlert } from '../../components/alerts/WarningAlert';
 
 export function Compare(): JSX.Element {
     const [modalShow, setModalShow] = useState<boolean>(false);
@@ -27,25 +28,27 @@ export function Compare(): JSX.Element {
     const handleModalClose = () => {
         setModalShow(false);
     };
-
     return (
         <div className={styles.CompareWrapper}>
-            <Button
-                className='w-25'
-                variant={isButtonDisabled ? 'secondary' : 'outline-dark'}
-                onClick={handleModalOpen}
-                disabled={isButtonDisabled}
-            >
-                Add Players
-            </Button>
-            {isButtonDisabled && (
-                <Alert variant="warning" className="mt-2">
-                    You can only compare up to 3 players.
-                </Alert>
-            )}
+            <div className={styles.ButtonWrapper}>
+                <Button
+                    className='btn-sm'
+                    variant={isButtonDisabled ? 'secondary' : 'outline-dark'}
+                    onClick={handleModalOpen}
+                    disabled={isButtonDisabled}
+                >
+                    Add Players
+                </Button>
+
+                {isButtonDisabled && (
+                    <WarningAlert size="sm" message="Cannot Add More Than 3 Players"/>
+                )}
+            </div>
+
             <div className={styles.ComparedPlayersWrapper}>
                 <PlayerCompareTable selectedPlayers={selectedPlayers} />
             </div>
+
             <PlayerSearchModal 
                 onHide={handleModalClose}
                 show={modalShow}
