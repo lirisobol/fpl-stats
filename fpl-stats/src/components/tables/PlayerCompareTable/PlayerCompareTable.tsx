55ae11d318e0597456e06080e358d1f3c09e335a
Liri <lirisobol16@gmail.com>
2024-07-30 13:38:20 +0300

    added TableDef file in CompareTable for a more modular approach and concern separation

--- fpl-stats/src/components/tables/PlayerCompareTable/PlayerCompareTable.tsx
+++ fpl-stats/src/components/tables/PlayerCompareTable/PlayerCompareTable.tsx
@@ -2,18 +2,13 @@ import { useEffect, useRef, useState } from "react";
 import { AgGridReact } from "ag-grid-react";
 import 'ag-grid-community/styles/ag-grid.css'; 
 import 'ag-grid-community/styles/ag-theme-quartz.css';
-import { PlayerData, playerStatsModel } from "../../../models/Player";
-import { getCellClassRules } from "../../../utils/compareCellRules";
-import { PlayerRemoverHeader } from "./PlayerRemoverHeader/PlayerRemoverHeader";
+import { PlayerData} from "../../../models/Player";
 import { playersTableConfig } from "../../../utils/playersTableConfig";
 import { ColDef } from "ag-grid-community";
+import { generateCompareColDef, generateCompareRowDef } from "./TableDefs";
 
 interface PlayerCompareTableProps {
     selectedPlayers: PlayerData[];
-}
-interface RowData {
-    stat: string;
-    [key: string]: unknown; // This allows for dynamic keys and values
 }
 export function PlayerCompareTable({ selectedPlayers }: PlayerCompareTableProps): JSX.Element {
     const [columnDefs, setColumnDefs] = useState<ColDef[]>([]);
@@ -22,49 +17,27 @@ export function PlayerCompareTable({ selectedPlayers }: PlayerCompareTableProps)
     const gridApiRef = useRef<AgGridReact>(null); 
 
     useEffect(() => {
-        console.log(selectedPlayers);
-
-        if (selectedPlayers.length > 0) {
-            const columns: ColDef[] = [
-                { headerName: "Stat", field: "stat", pinned: 'left', width: 110 },
-                ...selectedPlayers.map((player, index) => ({
-                    headerName: `${player.first_name} ${player.second_name}`,
-                    field: `player${index}`,
-                    flex: 1,
-                    cellClassRules: getCellClassRules(selectedPlayers.length),
-                    suppressHeaderFilterButton: true,
-                    sortable: false,
-                    headerComponent: PlayerRemoverHeader,
-                    headerComponentParams: {
-                        playerId: player.id,
-                        playerName: `${player.first_name} ${player.second_name}`
-                    }
-                } as ColDef)),
-            ];
-            setColumnDefs(columns);
-
-            const rows = playerStatsModel
-                .filter(stat => stat.name !== "player_name")
-                .map(stat => {
-                    const row: RowData = { stat: stat.label };
-                    selectedPlayers.forEach((player, index) => {
-                        const value = player[stat.name];
-                        row[`player${index}`] = isNaN(Number(value)) ? value : Number(value);
-                    });
-                    return row;
-                });
-
-            setRowData(rows);
-
-            // Force grid update
-            if (gridApiRef.current) {
-                gridApiRef.current.api.refreshCells();
+        if (selectedPlayers.length <= 3) {
+            if (selectedPlayers.length > 0) {
+                const columns = generateCompareColDef(selectedPlayers);
+                setColumnDefs(columns);
+                const rows = generateCompareRowDef(selectedPlayers)
+                setRowData(rows);
+    
+                // Force grid update
+                if (gridApiRef.current) {
+                    gridApiRef.current.api.refreshCells();
+                }
+            } 
+            else {
+                setColumnDefs([]);
+                setRowData([]);
             }
-        } 
-        else {
-            setColumnDefs([]);
-            setRowData([]);
         }
+        else {
+            throw new Error('Cannot compare more than 3 players at once')
+        }
+
     }, [selectedPlayers]);
 
     return (
--- fpl-stats/src/pages/Compare/Compare.tsx
+++ fpl-stats/src/pages/Compare/Compare.tsx
@@ -4,12 +4,11 @@ import { Button } from 'react-bootstrap';
 import { PlayerSearchModal } from '../../components/modals/PlayerSearchModal/PlayerSearchModal';
 import { useAppSelector } from '../../hooks/redux-hooks';
 import { PlayerCompareTable } from '../../components/tables/PlayerCompareTable/PlayerCompareTable';
-import { RootState } from '../../store/store';
 import { PlayerData } from '../../models/Player';
 
 export function Compare(): JSX.Element {
     const [modalShow, setModalShow] = useState<boolean>(false);
-    const selectedPlayersFromRedux = useAppSelector((state: RootState) => state.compare.selectedPlayers ?? []);
+    const selectedPlayersFromRedux = useAppSelector((state) => state.compare.selectedPlayers ?? []);
     const [selectedPlayers, setSelectedPlayers] = useState<PlayerData[]>(selectedPlayersFromRedux);
 
     useEffect(() => {
@@ -17,20 +16,12 @@ export function Compare(): JSX.Element {
     }, [selectedPlayersFromRedux]);
 
     const handleModalOpen = () => {
-        console.log('inside handleModalOpen before:',typeof modalShow);
         setModalShow(true);
-        console.log('inside handleModalOpen after:',typeof modalShow);
-        
     };
 
     const handleModalClose = () => {
-        console.log('inside handleModalClose, before:',typeof modalShow);
         setModalShow(false);
-        console.log('inside handleModalClose, after:',typeof modalShow);
-
     };
-    console.log(typeof modalShow);
-    
     return (
         <div className={styles.CompareWrapper}>
             <Button
