b4de829242ae53d9d9d9970c9e196cc4966db4f5
Liri <lirisobol16@gmail.com>
2024-07-31 10:02:15 +0300

    added netlify.toml config for cors handling

--- fpl-stats/src/utils/appConfig.ts
+++ fpl-stats/src/utils/appConfig.ts
@@ -1,4 +1,5 @@
 class AppConfig {
     public readonly GeneralInformationEndpoint = 'https://fantasy.premierleague.com/api/bootstrap-static/';
+    public readonly CORS_PROXY = "https://api.allorigins.win/get?url=";
 }
 export const appConfig = new AppConfig();
--- fpl-stats/src/utils/axiosConfig.ts
+++ fpl-stats/src/utils/axiosConfig.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
-import { appConfig } from "./appConfig";
 
 // Base URL for your Express server
 const api = axios.create({
-  baseURL: appConfig.GeneralInformationEndpoint,
   headers: {
     'Content-Type': 'application/json',
   },
