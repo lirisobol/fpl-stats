141828bddd85dd942d16093341d54a907bb36481
Liri <lirisobol16@gmail.com>
2024-08-13 16:56:01 +0300

    board and player buttons styling

--- fpl-stats/src/components/draft/DraftPlayer/DraftPlayer.tsx
+++ fpl-stats/src/components/draft/DraftPlayer/DraftPlayer.tsx
@@ -1,23 +1,23 @@
-import { faPlus, faUser, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPlus} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
-import { Button } from "react-bootstrap";
 import { PlayerDraftModal } from "../../modals/PlayerDraftModal/PlayerDraftModal";
 import styles from "./DraftPlayer.module.scss";
 import { PlayerData } from "../../../models/general-info/Player";
 import { useAppDispatch } from "../../../hooks/redux-hooks";
 import { removeDraftPlayer } from "../../../store/slices/draftSlice";
+// import LIV from "../../../assets/images/kits/14.png";
 
 interface DraftPlayerProps {
     arrayIndex: number;
     player: PlayerData | null;
-    positionType: number; // Make sure to pass and use this if needed for specific behavior
+    positionType: number; 
 }
 
 export function DraftPlayer({ arrayIndex, player, positionType }: DraftPlayerProps): JSX.Element {
     const [modalShow, setModalShow] = useState(false);
     const dispatch = useAppDispatch();
-
+    
     const handleModalOpen = () => {
         setModalShow(true);
     };
@@ -29,28 +29,27 @@ export function DraftPlayer({ arrayIndex, player, positionType }: DraftPlayerPro
     const handleRemovePlayer = () => {
         dispatch(removeDraftPlayer(arrayIndex));
     };
-
+    const jerseyImagePath = player ? `/assets/images/kits/${player.team_code}.png` : '/assets/images/kits/default.png';
+    
     return (
         <div className={styles.DraftPlayerWrapper}>
             {player ? (
                 <div className={styles.PlayerInfo}>
-                    <Button variant="success" onClick={handleModalOpen}>
-                        {`${player.first_name} ${player.second_name}`} <FontAwesomeIcon icon={faUser} />
-                    </Button>
-                    <Button variant="danger" onClick={handleRemovePlayer}>
-                        <FontAwesomeIcon icon={faTrash} />
-                    </Button>
+                    <button className={styles.PlayerButton} onClick={handleRemovePlayer}>
+                        <img src={jerseyImagePath} alt={`${player.team_code} Jersey`}/>
+                        <div className={styles.PlayerName}>{player.first_name} {player.second_name}</div>
+                    </button>
                 </div>
             ) : (
-                <Button variant={'outline-dark'} onClick={handleModalOpen}>
-                    <FontAwesomeIcon icon={faPlus} style={{ padding: 12 }}/>
-                </Button>
+                <button className={styles.EmptyPlayer} onClick={handleModalOpen}>
+                    <FontAwesomeIcon icon={faPlus} style={{ padding: 12 ,width:30, height:30}}/>
+                </button>
             )}
             
             <PlayerDraftModal
                 onHide={handleModalClose}
                 show={modalShow}
-                positionType={positionType} // Make sure this prop is correctly used in the modal
+                positionType={positionType}
                 arrayIndex={arrayIndex}
             />
         </div>
